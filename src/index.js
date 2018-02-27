e7debcdb2c9d170952357dac747a841861728ce3
Viktor <vprevari@gmail.com>
2018-02-28 00:13:57 +0100

    Throttle search speed using lodash library

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React , {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -34,9 +35,10 @@ class App extends Component {
   }
 
   render () {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)  }, 300);
     return (
       <div>
-        <Searchbar onSearchTermChange={term => this.videoSearch(term)}/>
+        <Searchbar onSearchTermChange={videoSearch}/>
         <VideoDetail video={this.state.selectedVideo}/>
         <Videolist
         onVideoSelect={selectedVideo => this.setState({selectedVideo})  }
